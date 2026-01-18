5a0a7a9425be148ae112cc997e6ebab120363040
Vikas Singh <s.vikas892354@gmail.com>
2026-01-18 21:12:57 +0530

    Add dotenv support and update release notes
    - Introduced dotenv for environment variable management.
    - Updated .gitignore to include .env file.
    - Refactored GEMINI_API_KEY retrieval to use process.env.
    - Created release notes file detailing improvements and technical changes.
    - Added dotenv as a dependency in package.json and package-lock.json.

--- AutomateReleaseNotes/src/getGenAiInstance.js
+++ AutomateReleaseNotes/src/getGenAiInstance.js
@@ -4,7 +4,8 @@ import { GoogleGenAI } from '@google/genai';
 let genAIInstance;
 
 function getGenAiInstance() {
-  const  GEMINI_API_KEY = "AIzaSyDg8Tarq_rJKFnf9RjpAuA6lIGpzjQlzyg";
+  
+  const  GEMINI_API_KEY = process.env.GEMINI_API_KEY;
   if (!genAIInstance) {
     if (GEMINI_API_KEY) {
       genAIInstance = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
